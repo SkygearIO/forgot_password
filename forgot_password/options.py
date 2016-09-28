40673eea0b6fc6bd29f679c29da670bd4d83211f
Rick Mak <rick.mak@gmail.com>
2016-09-28 16:41:49 +0800

    Convert to use skygear.settings
    refs #5

--- forgot_password/options.py
+++ forgot_password/options.py
@@ -11,52 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
+from skygear.settings import SettingsParser, add_parser, settings
 
-from skygear.options import options as skyoptions
+parser = SettingsParser('FORGOT_PASSWORD')
 
+parser.add_setting('enable', atype=bool, default=False)
+parser.add_setting('appname', env_var='APP_NAME')
+parser.add_setting('url_prefix', default='http://127.0.0.1:3000')
+parser.add_setting('sender', default='no-reply@skygeario.com')
+parser.add_setting('smtp_host', required=False)
+parser.add_setting('smtp_port', atype=int, default=25)
+parser.add_setting('smtp_mode', default='normal')
+parser.add_setting('smtp_login', required=False)
+parser.add_setting('smtp_password', required=False)
 
-class Namespace:
-    @property
-    def appname(self):
-        return os.getenv('FORGOT_PASSWORD_APPNAME', skyoptions.appname)
+add_parser('forgot_password', parser)
 
-    @property
-    def url_prefix(self):
-        url_prefix = os.getenv('FORGOT_PASSWORD_URL_PREFIX',
-            os.getenv('URL_PREFIX', skyoptions.skygear_endpoint))  # noqa
-        if url_prefix.endswith('/'):
-            url_prefix = url_prefix[:-1]
-        return url_prefix
-
-    @property
-    def sender(self):
-        return os.getenv('FORGOT_PASSWORD_SENDER', 'no-reply@skygeario.com')
-
-    @property
-    def subject(self):
-        return os.getenv('FORGOT_PASSWORD_SUBJECT',
-                         'Reset password instructions')
-
-    @property
-    def smtp_host(self):
-        return os.getenv('SMTP_HOST')
-
-    @property
-    def smtp_port(self):
-        return int(os.getenv('SMTP_PORT', '25'))
-
-    @property
-    def smtp_mode(self):
-        return os.getenv('SMTP_MODE', 'normal')
-
-    @property
-    def smtp_login(self):
-        return os.getenv('SMTP_LOGIN')
-
-    @property
-    def smtp_password(self):
-        return os.getenv('SMTP_PASSWORD')
-
-
-options = Namespace()
+options = settings.forgot_password
