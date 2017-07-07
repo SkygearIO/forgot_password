77e680d2870434cc9837e51ec9c1fac0f030c489
Ben Lei <benlei@oursky.com>
2017-07-07 16:57:51 +0800

    Support send test email

--- forgot_password/handlers/forgot_password.py
+++ forgot_password/handlers/forgot_password.py
@@ -14,11 +14,14 @@
 
 
 import logging
+from collections import namedtuple
 from datetime import datetime
 
 import skygear
 from skygear import error as skyerror
 from skygear.error import SkygearException
+from skygear.models import Record, RecordID
+from skygear.utils.context import current_context
 from skygear.utils.db import conn
 
 from .template import FileTemplate, StringTemplate
@@ -46,6 +49,44 @@ def register_op(**kwargs):
     template_provider = kwargs['template_provider']
     settings = kwargs['settings']
     smtp_settings = kwargs['smtp_settings']
+
+    def send_mail(sender,
+                  email,
+                  subject,
+                  text_template_string=None,
+                  html_template_string=None,
+                  reply_to=None,
+                  template_params={}):
+
+        text_template = None
+        html_template = None
+        if text_template_string:
+            text_template = StringTemplate('reset_email_text',
+                                           text_template_string)
+            html_template = StringTemplate('reset_email_html',
+                                           html_template_string)
+        else:
+            text_template = template_provider.get_template('reset_email_text')
+            html_template = template_provider.get_template('reset_email_html')
+
+        try:
+            mailer = email_util.Mailer(
+                smtp_host=smtp_settings.host,
+                smtp_port=smtp_settings.port,
+                smtp_mode=smtp_settings.mode,
+                smtp_login=smtp_settings.login,
+                smtp_password=smtp_settings.password,
+            )
+            mailer.send_mail(sender,
+                             email,
+                             subject,
+                             text_template.render(**template_params),
+                             html=html_template.render(**template_params),
+                             reply_to=reply_to)
+        except Exception as ex:
+            logger.exception('An error occurred sending reset password'
+                             ' email to user.')
+            raise SkygearException(str(ex), skyerror.UnexpectedError)
 
     @skygear.op('user:forgot-password')
     def forgot_password(email):
@@ -97,31 +138,53 @@ def register_op(**kwargs):
                 'user_record': user_record,
             }
 
-            text = template_provider.\
-                get_template('reset_email_text').\
-                render(**template_params)
-            html = template_provider.\
-                get_template('reset_email_html').\
-                render(**template_params)
-
-            sender = settings.sender
-            reply_to = settings.reply_to
-            subject = settings.subject
-
-            try:
-                mailer = email_util.Mailer(
-                    smtp_host=smtp_settings.host,
-                    smtp_port=smtp_settings.port,
-                    smtp_mode=smtp_settings.mode,
-                    smtp_login=smtp_settings.login,
-                    smtp_password=smtp_settings.password,
-                )
-                mailer.send_mail(sender, user.email, subject, text,
-                                 html=html, reply_to=reply_to)
-                logger.info('Successfully sent reset password email to user.')
-            except Exception as ex:
-                logger.exception('An error occurred sending reset password'
-                                 ' email to user.')
-                raise SkygearException(str(ex), skyerror.UnexpectedError)
+            send_mail(settings.sender,
+                      user.email,
+                      settings.subject,
+                      reply_to=settings.reply_to,
+                      template_params=template_params)
 
             return {'status': 'OK'}
+
+    @skygear.op('user:forgot-password:test', key_required=True)
+    def test_forgot_password_email(email,
+                                   text_template=None,
+                                   html_template=None):
+        access_key_type = current_context().get('access_key_type')
+        if not access_key_type or access_key_type != 'master':
+            raise SkygearException(
+                'master key is required',
+                skyerror.AccessKeyNotAccepted
+            )
+
+        url_prefix = settings.url_prefix
+        if url_prefix.endswith('/'):
+            url_prefix = url_prefix[:-1]
+
+        dummy_user = namedtuple('User', ['id', 'email'])(
+            'dummy-id',
+            'dummy-user@example.com')
+
+        dummy_record_id = RecordID('user', 'dummy-id')
+        dummy_record = Record(dummy_record_id, dummy_record_id.key, None)
+
+        template_params = {
+            'appname': settings.app_name,
+            'code': 'dummy-reset-code',
+            'url_prefix': url_prefix,
+            'link': '{}/example-reset-password-link'.format(url_prefix),
+            'user_record': dummy_record,
+            'user': dummy_user,
+            'email': dummy_user.email,
+            'user_id': dummy_user.id
+        }
+
+        send_mail(settings.sender,
+                  email,
+                  settings.subject,
+                  reply_to=settings.reply_to,
+                  text_template_string=text_template,
+                  html_template_string=html_template,
+                  template_params=template_params)
+
+        return {'status': 'OK'}
